945a6ce3296b94eb03be10c97ce7fde6c2f1cfd4
AlexBall03 <alexanderdball03@gmail.com>
2022-10-10 15:45:17 -0700

    Reorganized the auth pages and routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,24 @@
 import './App.css';
-import Auth from './Pages/Auth/Auth';
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
+
+// Pages
+import Login from "./Pages/Auth/Login/Login";
+import Signup from "./Pages/Auth/Signup/Signup";
+// import Dashboard from './Pages/Dashboard/Dashboard';
 
 function App() {
   return (
-    <div className="App">
-      {/* ----- Pages ----- */}
-      {/* Authentication (Log In / Sign Up) */}
-      <Auth />
-
-      {/* Dashboard */}
-
-      {/* Projects */}
-
-      {/* Profile */}
-    </div>
+    <Router>
+      <div className="App">
+        {/* ----- Pages ----- */}
+        <Routes>
+          <Route path="/" element={<Navigate to="/login" />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          {/* <Route path="/dashboard" element={<Dashboard />} /> */}
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
