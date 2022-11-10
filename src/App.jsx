d8ba188dee4b0cf9ee0992ba2d53d4695ca4ab0b
AlexBall03 <alexanderdball03@gmail.com>
2022-11-10 14:02:24 -0700

    Reset project

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,7 @@
-import './App.css';
-import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
-
-// Pages
-import Login from "./Pages/Auth/Login/Login";
-import Signup from "./Pages/Auth/Signup/Signup";
-import Dashboard from './Pages/Dashboard/Dashboard';
+import "./App.css";
 
 function App() {
-  return (
-    <Router>
-      <div className="App">
-        {/* ----- Pages ----- */}
-        <Routes>
-          <Route path="/" element={<Navigate to="/login" />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-        </Routes>
-      </div>
-    </Router>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
